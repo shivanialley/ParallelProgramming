3f1fc22326c7fb28634612dada194426e0c0ded2
Shivani <165663972+shivanialley@users.noreply.github.com>
2024-05-05 12:00:14 +0530

    Add files via upload

---
+++ dayOne/modNPkg/myPkg/moduleIntro.py
@@ -0,0 +1,25 @@
+"""
+
+    It can be used as multiline comment / doc string
+    Intro to Modules in Python
+    """
+
+dataOne = 1000
+dataTwo = 'Some string introduction to modules'
+
+def funOne():
+    '''
+    funOne() part of moduleIntro
+    '''
+    print(f'funOne()...{__name__}')
+
+def funTwo():
+    '''
+    funTwo() part of moduleIntro
+    '''
+    print(f'funTwo()...{__name__}')
+
+if __name__ =='__main__':
+    funOne()
+    funTwo()
+    print(dataOne,dataTwo)
